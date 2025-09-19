a6546f0294dc08beb47061c7f36d92f0890fa812
Roberttheone <38722012+Roberttheone@users.noreply.github.com>
2025-09-19 11:55:12 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,10 @@
-export default { images:{ remotePatterns:[{protocol:'https',hostname:'images.metmuseum.org'},{protocol:'https',hostname:'upload.wikimedia.org'}] } }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.metmuseum.org' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' }
+    ]
+  }
+};
+export default nextConfig;
